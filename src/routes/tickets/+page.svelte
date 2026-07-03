1a14ba8ee05d38ae30902e5a5849f8869b44d22b
Mike <63655075+MixelTe@users.noreply.github.com>
2026-07-03 16:46:17 +0300

    update tickets page content

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -4,6 +4,9 @@
 
 <h1 class="h1">Виды билетов</h1>
 <h2 class="heading h3">Выбери подходящий</h2>
+<div class="text">
+	<p class="">Продажа билетов осуществляется в группе в ВК или телеграмм канале</p>
+</div>
 <Tickets />
 
 <style>
@@ -26,5 +29,18 @@
 		text-align: center;
 		text-transform: uppercase;
 		padding: 0.5em 0;
+	}
+
+	.text {
+		padding: clamp(2.5rem, 1.1rem + 5.6vw, 6rem) 4em 0 4em;
+		background-color: var(--theme-accent-bg);
+		color: var(--theme-accent-text);
+		transition: var(--theme-transition);
+	}
+	.text p {
+		font-size: clamp(1.375rem, 1.025rem + 1.4vw, 2.25rem);
+		max-width: 725px;
+		margin: auto;
+		text-align: center;
 	}
 </style>
